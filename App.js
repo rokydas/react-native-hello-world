f9e29da4c1df40cf7b4a0ce850b9d6b5adf77d87
rokydas <droky136@gmail.com>
2021-01-20 20:18:42 +0600

    initialization

--- App.js
+++ App.js
@@ -3,10 +3,17 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello React Native</Text>
+      <Text>I am Roky Das</Text>
+      <Text>I am learning React Native</Text>
+      <Text>for building mobile app</Text>
+      <Text>Hi SARUJ BRO. GIVE ME SOME SUGGESTIONS</Text>
+      <Text>Hey Showrav, How are you?</Text>
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -14,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgrey',
     alignItems: 'center',
     justifyContent: 'center',
   },
