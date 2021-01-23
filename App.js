695bf70024529c36147b4a4c32a8781be02a2a15
rokydas <droky136@gmail.com>
2021-01-23 14:19:54 +0600

    hook use done

--- App.js
+++ App.js
@@ -1,28 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert, StatusBar } from 'react-native';
+
+import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks";
 
 export default function App() {
+
+  // console.log(useDimensions());
+  // console.log(useDeviceOrientation());
+
+  const { portrait, landscape } = useDeviceOrientation();
 
   return (
     <View style={styles.container}>
       <Text>Hello React Native</Text>
-      <Text>I am Roky Das</Text>
-      <Text>I am learning React Native</Text>
-      <Text>for building mobile app</Text>
-      <Text>Hi SARUJ BRO. GIVE ME SOME SUGGESTIONS</Text>
-      <Text>Hey Showrav, How are you?</Text>
-      <StatusBar style="auto" />
+      <Image style={styles.image} source={require('./assets/roky.jpg')} />
+      {/* <StatusBar style="auto" /> */}
+      <Image style={styles.image} source={{ uri: "https://i.ibb.co/LvCMN10/psd-To-Html.jpg" }} />
+
+      <Button
+        title="Are you a React Developer?"
+        // onPress={() => alert("Button clicked")}
+        onPress={() => Alert.alert("Alert Title", "Alert Message",
+          [
+            { text: "Yes", onPress: () => console.log("Yes is clicked") },
+            { text: "No", onPress: () => console.log("No is clicked") },
+            { text: "May Be" },
+          ])
+        }
+      />
+
+      <Button
+        title="Are you a React Developer?"
+        // onPress={() => alert("Button clicked")}
+        onPress={() => Alert.prompt("Prompt Title", "Prompt Message", text => console.log(text))
+        }
+      />
+
+      <View style={{
+        width: '100%',
+        height: landscape ? '100%' : '30%',
+        backgroundColor: 'teal',
+      }}>
+        <Text style={{}}>I am under the blog</Text>
+      </View>
+
 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: 'lightgrey',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 10,
   },
+
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: 'cover'
+  },
+
+  // box: {
+  //   width: '100%',
+  //   height: landscape ? '30%' : '100%',
+  // },
+
 });
